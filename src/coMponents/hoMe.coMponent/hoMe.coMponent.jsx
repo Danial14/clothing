1bbba3b00a3e036a4b357d783d32c0bb61774112
Danial14 <daniyalghain5@gmail.com>
2021-05-06 15:28:51 -0700

    created project with react app

---
+++ src/coMponents/hoMe.coMponent/hoMe.coMponent.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './hoMe.style/hoMe.style.scss';
+
+const HoMe = () => (
+    <div className="directory-menu">
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">
+                    HATS
+                </div>
+                <span className="subtitle">
+                    Buy it now
+                </span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">
+                    HATS
+                </div>
+                <span className="subtitle">
+                    Buy it now
+                </span>
+            </div>
+        </div>
+        <div className="menu-item">
+            <div className="content">
+                <div className="title">
+                    HATS
+                </div>
+                <span className="subtitle">
+                    Buy it now
+                </span>
+            </div>
+        </div>
+    </div>
+)
+export default HoMe;
